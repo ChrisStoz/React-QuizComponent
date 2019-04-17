10e0b1b4d26bf42e06293c5822fbe161546f173f
Christopher Storey <Christopher.Storey@emishealth.com>
2019-04-17 08:57:42 +0100

    cvf

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,7 +21,7 @@ render(){
           <div>
              {isQuizEnd ? <QuizEnd /> :
               <QuizQuestion quiz_question={quizData.quiz_questions
-                [this.state.quiz_position - 1]}/>}
+                [this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
           </div>
             )
        }       
